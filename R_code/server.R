3d2c3970bb14836c78abadfb754614b96be2fd4c
jlashk <josiahkephart@gmail.com>
2016-12-06 19:59:49 -0500

    added simulated data

--- R_code/server.R
+++ R_code/server.R
@@ -19,7 +19,13 @@ shinyServer(function(input, output){
     
     
     # check if the last time is within the last hour or so, if not, give a warning message.
+    
+    
+    
+    
+    
     output$text1 <- renderText({     
+        
         df<- filedata()
         if (is.null(df)) return(NULL) # return nothing if nothing is uploaded
         df$temp= df[,2]
@@ -32,20 +38,64 @@ shinyServer(function(input, output){
         
         # compare the system time to the last time in the data
         diff <- as.numeric(difftime(Sys.time(), tail(sums1a$times, 1),  units = "mins"))
+        
+        
+        # we should differentiate the errors, or just have a single error message. 
+        if(diff > 60){
+            "Error! Logger stopped functioning more than an hour ago"
+            
+        }
+        
+    })
+    
+    
+    output$text1b <- renderText({     
+        
+        df<- filedata()
+        if (is.null(df)) return(NULL) # return nothing if nothing is uploaded
+        df$temp= df[,2]
+        
+        tformats <- c("%m/%d%y %H:%M:%S","%m/%d%y %H:%M","%Y-%m-%d %H:%M:%S") #known date formats in raw data
+        df$times = parse_date_time(df$Time, tformats, truncated = 1, tz="America/Lima")
+        
+        
+        sums1a = subset.data.frame(df, select = c(times,temp))
+        
         # check for missing values
         sums1a <- sums1a %>%
             mutate(temp = ifelse(temp == "blank", "NA", temp))
         nas<-sum(is.na(sums1a$temp))
-        # check for extremely low values
-        min_temp <- sum(sums1a$temp< -20, na.rm = TRUE)
         
-        if(diff > 60 | nas>0 | min_temp>0){
-            "Error! Logger stopped functioning more than an hour ago"
-            #}else if (nas>0){
-            #"There are missing values in the data"
+        if(nas > 0){
+            "Error! There are missing values"
         }
         
+        
     })
+    
+    
+    output$text1c <- renderText({     
+        
+        df<- filedata()
+        if (is.null(df)) return(NULL) # return nothing if nothing is uploaded
+        df$temp= df[,2]
+        
+        tformats <- c("%m/%d%y %H:%M:%S","%m/%d%y %H:%M","%Y-%m-%d %H:%M:%S") #known date formats in raw data
+        df$times = parse_date_time(df$Time, tformats, truncated = 1, tz="America/Lima")
+        
+        
+        sums1a = subset.data.frame(df, select = c(times,temp))
+        
+        min_temp <- sum(sums1a$temp< -30, na.rm = TRUE)
+        
+        
+        if(min_temp > 0){
+            "Error! The temperature fell below 30 degees Celcius"
+        }
+    })
+    
+    
+    
     
     # check to see if the logger reaches a sum above 85 degrees
     output$text2 <- renderText({ 
@@ -79,10 +129,11 @@ shinyServer(function(input, output){
         sums1a = subset.data.frame(df, select = c(times,temp))
         sums1a <- na.omit(sums1a)
         
+        
         # a function that identifies the temp peaks, when the stove
         # has been used
-        stove_uses <- function(x, t = 25, h = 1){
-            a<-x %>% mutate(peak = ifelse(as.numeric(temp) > t, 1,0))
+        stove_uses <- function(x, tem = 25, h = 1){
+            a<-x %>% mutate(peak = ifelse(as.numeric(temp) > tem, 1,0))
             a$peak2 <- 0
             a$peak3 <- 0
             for (i in 1:(length(a$peak)-1)){
@@ -111,16 +162,80 @@ shinyServer(function(input, output){
             # add the final peak to the dataset
             a$peak3[a$peak2 == max(a$peak2)] = 1 
             a
+            #peaknum
         }
         
-        # output to plot
-        b<- stove_uses(sums1a, t = 24, h = 3 )
+        
+        # Use the stove_uses function and make the plots
+        b<- stove_uses(sums1a, tem = 30, h = 3 )
         
         
-        plot(b$times, b$temp, type = "l", main = "Stove Temperature")
-        points(b$times, b$peak3*24, col = "red")
+        if(input$time == 1){
+            
+            begin<- tail(b$times, 1) - as.difftime(1, unit = "weeks")
+            c<- b %>% filter(times > begin)
+            plot(c$times, c$temp, type = "l", main = "Stove Temp Within the Last Week")
+            # the red peak marks that aren't used to mark peaks sit at zero. We can either ignore
+            # that or delete them bu turning them into NA. Method below just called them NA, doenst work.
+            #c$peak3[c$peak3 == 0]<-"NA"
+            
+            points(c$times, c$peak3*30, col = "red")
+            
+            output$text3 <- renderText({
+                paste("The stove was used ", sum(c$peak3, na.rm = T), " times in the last week. These
+                      are marked with a red dot on the plot.")
+            })
+            
+            }
+        
+        
+        if(input$time == 2){
+            
+            begin<- tail(b$times, 1) - as.difftime(30, unit = "days")
+            c<- b %>% filter(times > begin)
+            plot(c$times, c$temp, type = "l", main = "Stove Temp Within the Last Month")
+            points(c$times, c$peak3*30, col = "red")
+            # prints out how many times the stove was used in the last period of time
+            output$text3 <- renderText({
+                paste("The stove was used ", sum(c$peak3, na.rm = T), " times in the last month. These
+                      are marked with a red dot on the plot.")
+            })
+            
+            }
+        
+        if(input$time == 3){
+            
+            begin<- tail(b$times, 1) - as.difftime(90, unit = "days")
+            c<- b %>% filter(times > begin)
+            plot(c$times, c$temp, type = "l", main = "Stove Temp in the Last 3 Months")
+            points(c$times, c$peak3*30, col = "red")
+            
+            output$text3 <- renderText({
+                paste("The stove was used ", sum(c$peak3, na.rm = T), " times in the last three months. These
+                      are marked with a red dot on the plot.")
+            })
+            
+            }
+        
+        if(input$time == 4){
+            
+            plot(b$times, b$temp, type = "l", main = "Stove Temp Over All Time")
+            points(b$times, b$peak3*30, col = "red")
+            
+            output$text3 <- renderText({
+                paste("The stove was used ", sum(b$peak3, na.rm = T), " times during the whole period. These
+                      are marked with a red dot on the plot.")
+            })
+            
+            }
+        
+        
         
     })
     
-})
+    
+        })
+
+
+
 
--- R_code/ui.R
+++ R_code/ui.R
@@ -1,31 +1,37 @@
 library(shiny)
 
-
-#shinyUI(pageWithSidebar(
-    
-   # titlePanel("Stove Temperature Info"),
-    
-    #Selector for file upload
-    #fileInput('datafile', 'Upload CSV file',
-             # accept=c('text/csv')),
-    
-       # mainPanel(
-            #textOutput("text1")
-            #plotOutput("plot1")
-        #)
-#))
-
-
 shinyUI(fluidPage(
     titlePanel("Stove Temperature Info"),
     
+    # drop down menu with time selections
+    
+    selectInput("time", label = h3("Time Period"),
+                choices = list("Week" = 1, "Month" = 2, "Three Months" = 3, "All Time" = 4),
+                selected = 1),
+    
+    #fluidRow(column(3, verbatimTextOutput("value"))),
+    
     sidebarLayout(
-        sidebarPanel(fileInput('datafile', 'Upload CSV file',
-                                   accept=c('text/csv'))),
+        
+        
+        
+        sidebarPanel(
+            fileInput('datafile', 'Upload CSV file', accept=c('text/csv'))),
+        
+        # selectInput("select", label = h3("Time Period"),
+        #choices = list("Week" = 1, "Month" = 2, "Three Months" = 3, "All Time" = 4),
+        # selected = 1),
+        
+        
+        
         mainPanel(
             textOutput("text1"),
+            textOutput("text1b"),
+            textOutput("text1c"),
             textOutput("text2"),
-            plotOutput("plot1")
-            )
+            plotOutput("plot1"),
+            textOutput("text3")
+            
+        )
     )
 ))
